bea3f34f7cf9a5dfe35361ea8919e6d4ad36fc21
Bruno <37672571+sharkb8i@users.noreply.github.com>
2020-11-09 17:42:49 -0300

    SVM deu pau

--- svm.py
+++ svm.py
@@ -27,16 +27,16 @@ def svm(cnj1, cnj2):
     features_cnj1 = features_cnj1.drop(['Class'], axis=1)
     features_cnj1 = features_cnj2.drop(['Class'], axis=1)
 
-    clf_poly_C, clf_rbf_C = findBestC(features_cnj1, target_cnj1, features_cnj2, target_cnj2)
-    
-    return [clf_poly_C, clf_rbf_C] #retorna os svm com os kernels poly e rbf
+    clf_poly, clf_rbf = findBestC(features_cnj1, target_cnj1, features_cnj2, target_cnj2)
+
+    return [clf_poly, clf_rbf #retorna os svm com os kernels poly e rbf
     
 # Função pra encontrar a melhor reta de divisão
 def findBestC(train_features, train_target, validation_features, validation_target):
     i = 0.1
     best_poly = [None,0] # Classifier, Score
-    best_rbf  = [None,0]
-    best      = [0,0]
+    best_rbf  = [None,0] # Classifier, Score
+    best_acc      = [0,0]
     while i <= 20:
         clf_poly = make_pipeline(StandardScaler(), SVC(gamma='scale', kernel='poly', C=i))
         clf_poly.fit(train_features, train_target)
@@ -44,14 +44,14 @@ def findBestC(train_features, train_target, validation_features, validation_targ
         clf_rbf  = make_pipeline(StandardScaler(), SVC(gamma='scale', kernel='rbf', C=i))
         clf_rbf.fit(train_features, train_target)
 
-        best[0] = clf_poly.score(validation_features, validation_target)
-        best[1] = clf_rbf.score(validation_features, validation_target)
+        best_acc[0] = clf_poly.score(validation_features, validation_target)
+        best_acc[1] = clf_rbf.score(validation_features, validation_target)
 
-        if(best_poly[1] < best[0]):
-            best_poly = [clf_poly, best[0]]
-        if(best_rbf[1] < best[1]):
-            best_rbf = [clf_rbf, best[1]]
+        if(best_poly[1] < best_acc[0]):
+            best_poly = [clf_poly, best_acc[0]]
+        if(best_rbf[1] < best_acc[1]):
+            best_rbf = [clf_rbf, best_acc[1]]
 
         i+=0.1
     
-    return (best_poly, best_rbf)
+    return (best_poly[0], best_rbf[0])
